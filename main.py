67597fe86c506bf2296272864be2b9b68c9cc5eb
rpdentrechales <rpdprocorpo@gmail.com>
2025-05-29 15:11:52 -0300

    update

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ def main():
 
     LARGURA, ALTURA = 700, 700
     JANELA = pygame.display.set_mode((LARGURA, ALTURA))
-    FPS = 60
+    FPS = 2
     clock = pygame.time.Clock()
     pygame.display.set_caption("primeiro jogo!")
 
-    quadrado = pygame.Rect((35,35),(1,1)).inflate(70,70)
-    posicoes_x = [35,105,175,245,315,385,455,525,595,665]
+    quadrado = pygame.Rect((0,0),(70,70))
     andar =  False
-    index_x = 0
+    indo = True
+
     rodar = True
+
 
     while rodar:
         for event in pygame.event.get():
@@ -26,11 +27,22 @@ def main():
         if tecla[pygame.K_RIGHT] or tecla[pygame.K_d]:
             andar =  True
          
-        if andar :
-            index_x += 1
-            if (index_x > len(posicoes_x)-1):
-                index_x = 0
-            quadrado.x = posicoes_x[index_x]
+        if andar:
+
+            if indo == True:
+                quadrado.x += 70
+
+            else:
+
+                quadrado.x -= 70
+
+            if quadrado.x >= LARGURA-70:
+
+                indo = False
+
+            if quadrado.x <= 0:
+                indo = True
+
         JANELA.fill("black")
 
         pygame.draw.rect(JANELA, "red", quadrado)
