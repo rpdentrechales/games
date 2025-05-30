c597c75dcb9d209b5491585636890094b8f07a61
rpdentrechales <rpdprocorpo@gmail.com>
2025-05-30 12:32:38 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 def main():
 
@@ -10,7 +12,12 @@ def main():
 
     quadrado = pygame.Rect((0,0),(70,70))
     andar =  False
-    indo = True
+
+    ir_para_direita = True
+    ir_para_esquerda = False
+    ir_para_cima = False
+    ir_para_baixo = False
+
 
     rodar = True
 
@@ -29,22 +36,55 @@ def main():
          
         if andar:
 
-            if indo == True:
+            if ir_para_direita == True:
                 quadrado.x += 70
 
-            else:
-
+            if ir_para_esquerda == True:
                 quadrado.x -= 70
+            
+            if ir_para_cima == True:
+                quadrado.y -= 70
+            
+            if ir_para_baixo == True:
+                quadrado.y += 70
 
             if quadrado.x >= LARGURA-70:
-
-                indo = False
+                
+                print("bateu na parede direita")
+                ir_para_direita = False
+                ir_para_esquerda = False
+                ir_para_cima = False
+                ir_para_baixo = True
+            
+            if quadrado.y >= ALTURA-70:
+                
+                print("bateu na parede de baixo")
+                ir_para_direita = False
+                ir_para_esquerda = True
+                ir_para_cima = False
+                ir_para_baixo = False
 
             if quadrado.x <= 0:
-                indo = True
+
+                print("bateu na parede da esquerda")
+                ir_para_direita = False
+                ir_para_esquerda = False
+                ir_para_cima = True
+                ir_para_baixo = False
+            
+            if quadrado.y <= 0 and quadrado.x == 0:
+
+                print("bateu na parede de cima")
+                ir_para_direita = True
+                ir_para_esquerda = False
+                ir_para_cima = False
+                ir_para_baixo = False
+
+        posicao = (quadrado.x, quadrado.y)
+        posicao_texto = my_font.render(f'{posicao}', False, "white")
 
         JANELA.fill("black")
-
+        JANELA.blit(posicao_texto, (LARGURA//2,ALTURA//2))
         pygame.draw.rect(JANELA, "red", quadrado)
         pygame.display.flip()
         clock.tick(FPS)
